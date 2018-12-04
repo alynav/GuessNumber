7b8f16c66e485618bba951da572505219ba90c8a
alina <alynavelea@gmail.com>
2018-12-04 22:40:09 +0200

    Game created

--- GuessNumber/Program.cs
+++ GuessNumber/Program.cs
@@ -10,6 +10,33 @@ namespace GuessNumber
     {
         static void Main(string[] args)
         {
+            int a = 0;
+            int b = 100;
+            int x = new Random().Next(a, b);
+            //Console.WriteLine(x); 
+
+            do
+            {
+                Console.WriteLine("Introduceti un numar intre {0} si {1}", a, b);
+                string str = Console.ReadLine();
+                int nr = int.Parse(str);
+                //daca nr > 
+                //daca nr <
+                //daca nr =
+                if (nr < x)
+                {
+                    Console.WriteLine("ati introdus un numar mai mic");
+                }
+                else if (nr > x)
+                {
+                    Console.WriteLine("ati introdus un numar mai mare");
+                }
+                else
+                {
+                    Console.WriteLine("ati ghicit numarul");
+                    break;
+                }
+            } while (true);
         }
     }
 }
